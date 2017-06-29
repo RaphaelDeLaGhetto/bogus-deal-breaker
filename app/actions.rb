16b20b483f72a94e7c9e51b2ad921da7d457d06c
Daniel Bidulock <daniel@capitolhill.ca>
2017-06-29 01:37:56 +0000

    Get form and validation demo for W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,33 @@
 get '/' do
-  @posts = Post.all
+  @posts = Post.order(created_at: :desc)
   erb :index
+end
+
+
+get '/new' do
+  @post = Post.new
+  erb :new
+end
+
+post '/post' do
+  @post = Post.new
+  
+  @post.description = params[:description]
+  
+  # Move file into storage
+    # Move file into storage
+  if params[:image]
+    tempfile = params[:image][:tempfile] 
+    filename = params[:image][:filename] 
+    filepath = "uploads/#{filename}"
+    @post.image = filepath
+    filepath = "public/#{filepath}"
+    FileUtils.cp(tempfile.path, filepath)
+  end
+  
+  if @post.save
+    redirect '/'
+  else
+    erb :new
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
     belongs_to :user
     
+    validates :description, presence: true
+    validates :image, presence: true
+    
     def get_freshness
        if self.created_at > DateTime.now.prev_day 
            "Go get some now!"
