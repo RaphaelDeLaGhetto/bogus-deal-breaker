1fd0ad4919a328447dffc32ffc1ef4d6f887aebd
Daniel Bidulock <daniel@capitolhill.ca>
2017-07-06 01:34:20 +0000

    Get demo for W5D2

--- app/actions.rb
+++ app/actions.rb
@@ -4,28 +4,36 @@ helpers do
   end
 end
 
+#
+# GET /
+#
 get '/' do
   @posts = Post.order(created_at: :desc)
   erb :index
 end
 
-
+#
+# GET /new
+#
 get '/new' do
   @post = Post.new
   erb :new
 end
 
+#
+# POST /post
+#
 post '/post' do
   @post = Post.new
   
   @post.description = params[:description]
+  @post.user_id = session[:user_id]
   
   # Move file into storage
-    # Move file into storage
   if params[:image]
     tempfile = params[:image][:tempfile] 
     filename = params[:image][:filename] 
-    filepath = "uploads/#{filename}"
+    filepath = "/uploads/#{filename}"
     @post.image = filepath
     filepath = "public/#{filepath}"
     FileUtils.cp(tempfile.path, filepath)
@@ -37,6 +45,44 @@ post '/post' do
     erb :new
   end
 end
+
+#
+# GET /post/:id
+#
+get '/post/:id' do
+  @post = Post.find(params[:id])
+  erb :show
+end
+
+#
+# GET /post/:id/edit
+#
+get '/post/:id/edit' do
+  @post = Post.find(params[:id])
+  erb :edit
+end
+
+#
+# PATCH /post/:id
+#
+patch '/post/:id' do
+  @post = Post.find(params[:id])
+  @post.description = params[:description]
+  if @post.save
+    redirect "/post/#{@post.id}"
+  else
+    erb :edit 
+  end
+end
+
+#
+# DELETE /post/:id
+#
+delete '/post/:id' do
+  Post.delete(params[:id])
+  redirect('/')
+end
+
 
 
 get '/login' do
